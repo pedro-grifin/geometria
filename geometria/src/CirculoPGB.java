8ed5de075580d0185cf0a26cb0a9a845f3696f92
Pedro Guzman <pedro.grifin@gmail.com>
2020-02-04 21:43:44 +0100

    Siempre positivo

--- geometria/src/CirculoPGB.java
+++ geometria/src/CirculoPGB.java
@@ -6,6 +6,7 @@ public class Circulo extends FiguraGeometrica {
 	
 	public Circulo(double r, String tipoFigura) {
 		super(tipoFigura);
+		if(r<0) {r*=-1;}
 		radio = r;
 	}
 
--- geometria/src/RectanguloPGB.java
+++ geometria/src/RectanguloPGB.java
@@ -1,19 +1,38 @@
-
+/**
+ * 
+ * @author Pedro Guzman
+ * @version 1.2
+ *
+ */
 public class Rectangulo extends FiguraGeometrica {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Constructor de la clase
+	 * @param tipoFigura Descripcion de la figura
+	 * @param lG valor numerico del tamaño del lado grande
+	 * @param lP valor numerico del tamaño del lado pequeño
+	 */
 	public Rectangulo(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 * Clase que calcula el area del rectangulo.
+	 *@return devuelve el area del rectangulo
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * calcula el perimetro.
+	 * @return devuelve el perimetro.
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
